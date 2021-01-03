7dd9e20b4de85affb99ec47e3bbef65752e477b5
cybercdh <cdh.cert@gmail.com>
2021-01-03 08:41:13 +0000

    zyxel known plaintext attack

--- zyxel/zyxel.sh
+++ zyxel/zyxel.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Ta-daaaaa! key0=65a101b7, key1=40d2eb72, key2=2f98b039
 PKEXTRACT=/usr/local/Cellar/pkcrack/1.2.2/bin/extract
 
 FWNAME=$1
@@ -13,4 +12,3 @@ $PKEXTRACT $TEMP_ZIP $FWNAME.conf $FWNAME.plaintext
 rm $TEMP_ZIP
 
 pkcrack -C $FWNAME.bin -c db/etc/zyxel/ftp/conf/system-default.conf -p $FWNAME.plaintext -d cracked.zip -a
-
